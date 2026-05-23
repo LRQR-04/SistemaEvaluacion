84ff843f1d3166aa96bc43fedc1004b811159221
LRQR-04 <luisinformaticajs17@gmail.com>
2026-05-23 13:42:06 -0600

    Configuración inicial

---
+++ backend/app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from app.config.database import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="Sistema Escolar")
+
+# crear tablas automáticamente
+Base.metadata.create_all(bind=engine)
+
+# Configuración de CORS para permitir peticiones desde el frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# rutas
+# app.include_router(usuario_router)
+
+
+@app.get("/")
+def root():
+    return {"mensaje": "API funcionando bien"}
