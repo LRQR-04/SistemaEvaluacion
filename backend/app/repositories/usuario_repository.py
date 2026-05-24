994b6ba8073934f90a14f7b2866a3d17715d14e9
LRQR-04 <luisinformaticajs17@gmail.com>
2026-05-24 00:20:15 -0600

    Agrega función para obtener usuario por id

--- backend/app/repositories/usuario_repository.py
+++ backend/app/repositories/usuario_repository.py
@@ -8,3 +8,10 @@ def crear_usuario(db: Session, usuario: Usuario) -> Usuario:
     db.refresh(usuario)
 
     return usuario
+
+
+def obtener_usuario_por_id(
+    db: Session,
+    id_usuario: int,
+):
+    return db.query(Usuario).filter(Usuario.id_usuario == id_usuario).first()
